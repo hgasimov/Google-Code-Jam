ec5133452e53f375dc341d429c91506857f5765b
hgasimov <gasimov.huseyn@gmail.com>
2013-04-01 09:42:38 -0700

    Add BoxFactory

--- CodeJam/training/BoxFactory_Recursive.py
+++ CodeJam/training/BoxFactory_Recursive.py
@@ -106,6 +106,5 @@ for caseno in range(1, int(f.readline())+1):
     print('Case #' + str(caseno) + ': ' + str(res))
     fw.write('Case #' + str(caseno) + ': '  + str(res) + '\n')    
      
-
-
-
+fw.close()
+f.close()
